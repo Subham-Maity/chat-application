44c609ba063d7b0686b2e78fdd12ccfac59a5780
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-11 11:10:26 +0530

    Import SDK

---
+++ client/src/reactIntegration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SDK from "./SDK";
+
+function ReactIntegration() {
+  const initSDK = async () => {};
+
+  return (
+    <div>
+      <button onClick={initSDK}>InittialzeSDK</button>
+    </div>
+  );
+}
