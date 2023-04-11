cfd13fedf9f00320961a5dc570178f36ecb492f5
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-11 11:18:04 +0530

    connectionListener - sandbox add

--- client/src/reactIntegration.js
+++ client/src/reactIntegration.js
@@ -2,6 +2,13 @@ import React from "react";
 import SDK from "./SDK";
 
 function ReactIntegration() {
+  function connectionListener(response) {
+    if (response.status === "CONNECTED") {
+      console.log("Connection Established");
+    } else if (response.status === "DISCONNECTED") {
+      console.log("Disconnected");
+    }
+  }
   const initSDK = async () => {
     const initializeObj = {
       apiBaseUrl: `API_URL`,
