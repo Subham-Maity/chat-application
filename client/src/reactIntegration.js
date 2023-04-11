57a4d378d5d5f4e805951df967e9e18b2ddf5aed
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-11 11:11:56 +0530

    Request Params

--- client/src/reactIntegration.js
+++ client/src/reactIntegration.js
@@ -2,7 +2,15 @@ import React from "react";
 import SDK from "./SDK";
 
 function ReactIntegration() {
-  const initSDK = async () => {};
+  const initSDK = async () => {
+    const initializeObj = {
+      apiBaseUrl: `API_URL`,
+      licenseKey: `LICENSE_KEY`,
+      isTrialLicenseKey: `TRIAL_MODE`,
+      callbackListeners: {},
+    };
+    await SDK.initializeSDK(initializeObj);
+  };
 
   return (
     <div>
